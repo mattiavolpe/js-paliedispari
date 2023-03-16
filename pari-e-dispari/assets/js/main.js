8a2c4dc8d5ad489bfb2d6eb8646db2fed885fc20
mattiavolpe <64392380+MatthewFox90@users.noreply.github.com>
2023-03-16 17:04:27 +0100

    ADD Even / Odd 2nd implementation with controls

--- pari-e-dispari/assets/js/main.js
+++ pari-e-dispari/assets/js/main.js
@@ -1,15 +1,25 @@
+// TODO I WILL REMOVE IT AFTER CODING
+"use strict";
 /*
 Pari e Dispari
 L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
 */
 
-const evenOdd = prompt("Pari o dispari?");
+let evenOdd = prompt("Pari o dispari?");
+let evenOddToLowerCase = evenOdd.toLowerCase();
+while (evenOddToLowerCase != "pari" && evenOddToLowerCase != "dispari") {
+  alert('Assicurati di inserire "pari" o "dispari"');
+  evenOdd = prompt("Pari o dispari?");
+  evenOddToLowerCase = evenOdd.toLowerCase();
+}
 
-const userNumber = Number(prompt("Inserisci un numero da 1 a 5"));
-console.log(userNumber);
+let userNumber = Number(prompt("Inserisci un numero da 1 a 5"));
+while (userNumber < 1 || userNumber > 5 || (isNaN(userNumber))) {
+  alert("Assicurati di inserire un numero tra 1 e 5, estremi inclusi");
+  userNumber = Number(prompt("Inserisci un numero da 1 a 5"));
+}
 
 const pcNumber = Math.floor(Math.random() * 5) + 1;
-console.log(pcNumber);
 
 const somma = userNumber + pcNumber;
 console.log(somma);
