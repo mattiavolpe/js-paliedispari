bd4a57e67cd6dcf214c7ed25266286c53434a988
mattiavolpe <64392380+MatthewFox90@users.noreply.github.com>
2023-03-16 15:35:50 +0100

    ADD Palindrome second implementation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,3 @@
-// TODO I WILL DELETE IT AT THE END OF CODING
-"use strict";
-
 /*
 Palidroma
 Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
@@ -13,22 +10,52 @@ Scriviamo sempre in italiano i passaggi che vogliamo fare
 Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
 */
 
-// <---------- PALINDROM ---------->
+// <---------- PALINDROME ---------->
 // ASK THE USER TO INPUT THE WORD
-const word = prompt("Inserisci una parola");
+let word = prompt("Inserisci una parola");
 
-// SET A CHECK VARIABLE
-let isPalindrome = false;
+// CHECK IF THERE'S MORE THAN ONE WORD OR AN EMPTY INPUT
+while(checkIfSingleNonEmptyWord(word)) {
+  word = prompt("Inserisci una parola");
+}
+
+// CONVERT THE WORD TO LOWERCASE FOR NOT CASE SENSITIVE CHECKING
+const wordLowerCase = word.toLowerCase();
+console.log(wordLowerCase);
 
 // CYCLE THROUGH HALF THE WORD TO CHECK IF EACH LETTER MATCHES ITS "SAME INDEXED LETTER WHEN STARTING COUNTING FROM THE END"
-for (let i = 0; i < word.length / 2; i++) {
-  if (word[i] == word[word.length - 1 - i]) {
-    isPalindrome = true;
-  } else {
-    isPalindrome = false;
-    i = word.length / 2;
-  }
-}
+let isPalindrome = checkIfPalindrome(wordLowerCase);
 
 // CHECK VALUE OF ISPALINDROME
 isPalindrome ? console.log(`La parola ${word} è palindroma`) : console.log(`La parola ${word} non è palindroma`);
+
+// <---------- FUNCTIONS ---------->
+
+// FUNCTION TO CHECK IF THERE'S MORE THAN ONE WORD OR AN EMPTY INPUT
+function checkIfSingleNonEmptyWord(insertedWord) {
+  if (insertedWord == "") {
+    alert("Inserisci un'unica parola e non lasciare il campo vuoto");
+    return true;
+  } else {
+    for (let i = 0; i < insertedWord.length; i++) {
+      if (insertedWord[i] == " ") {
+        alert("Inserisci un'unica parola e non lasciare il campo vuoto");
+        return true;
+      }
+    }
+  }
+}
+
+// FUNCTION TO CYCLE THROUGH HALF THE WORD TO CHECK IF EACH LETTER MATCHES ITS "SAME INDEXED LETTER WHEN STARTING COUNTING FROM THE END"
+function checkIfPalindrome(wordToCheck) {
+  let checker = false;
+  for (let i = 0; i < wordToCheck.length / 2; i++) {
+    if (wordToCheck[i] == wordToCheck[wordToCheck.length - 1 - i]) {
+      checker = true;
+    } else {
+      checker = false;
+      i = wordToCheck.length / 2;
+    }
+  }
+  return checker;
+}
